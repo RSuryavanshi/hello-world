ee36175df654fcf0d45888c36d3dcd0cb14e081a
Suryavanshi <Rahul.Suryavanshi@spirent.com>
2018-06-08 14:57:07 +0530

    First commit..

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+
+
+print ("Hello World!!!")
+
+
+def hello() :
+    print ("My first github repository")    
+
+
+def sum(a, b) :
+    return a+b;
+
+
+hello()
+
+print "Adding two numbers"
+total = sum( 3, 4)
+print "Total:", total
