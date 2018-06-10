6bebe4b5d7b2729e35385c03ae589b41875e6e19
RSuryavanshi <Rahul.Suryavanshi@spirent.com>
2018-06-10 18:00:31 +0530

    Fix issues

--- unit_test/unit_test_script.py
+++ unit_test/unit_test_script.py
@@ -1,7 +1,12 @@
-#import sum from hello_world
-#import hello_world
 import sys
-#sys.path.append('../')
+import os
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+    print "Total:", path
+del path
+
 from hello_world import sum
 
 print "############# Unit Test Script"
@@ -14,3 +19,4 @@ print "Total:", total
 print "############# Test case 2"
 total = sum(10, 4)
 print "Total:", total
+
