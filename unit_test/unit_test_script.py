36c408bec8db9df1e934f9a89a90109cbe429cbc
RSuryavanshi <Rahul.Suryavanshi@spirent.com>
2018-06-10 18:02:56 +0530

    Fix unit Test

--- unit_test/unit_test_script.py
+++ unit_test/unit_test_script.py
@@ -4,7 +4,6 @@ import os
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if not path in sys.path:
     sys.path.insert(1, path)
-    print "Total:", path
 del path
 
 from hello_world import sum
