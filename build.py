9d574f1f556c80ae79b2b9e7d5070f900ba20008
RSuryavanshi <Rahul.Suryavanshi@spirent.com>
2018-06-10 17:32:58 +0530

    Fix issues

--- build.py
+++ build.py
@@ -9,11 +9,11 @@ class CiBuild(object):
     def __init__(self):
         self._root_dir = os.getcwd()
         print('Root directory %s' % self._root_dir)
-        self._unit_test_dir = os.path.normpath('%s\unit_test' % self._root_dir)
+        self._unit_test_dir = os.path.normpath('%s/unit_test' % self._root_dir)
         print('Unit test script location %s' % self._unit_test_dir)
         
     def run_unit_tests(self):
-        scriptToRun = 'python ' + self._unit_test_dir + "\unit_test_script.py"
+        scriptToRun = 'python ' + self._unit_test_dir + "/unit_test_script.py"
         #subprocess.Popen(scriptToRun, cwd=self._root_dir, shell=True)
         subprocess.Popen(scriptToRun, cwd=self._root_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print('Unit test script completed successfully')
